7f80065107d1bd2416aa63654d0b5bd99f9fa405
Bram Droppers <bram.droppers@wur.nl>
2018-01-23 10:56:01 +0100

    Update

--- vic/vic_run/include/vic_def.h
+++ vic/vic_run/include/vic_def.h
@@ -59,7 +59,7 @@
 #define ERROR        -999      /**< Error Flag returned by subroutines */
 
 /***** Define maximum array sizes for model source code *****/
-#define MAX_VEG         12     /**< maximum number of vegetation types per cell */
+#define MAX_VEG         40     /**< maximum number of vegetation types per cell */
 #define MAX_LAYERS      3      /**< maximum number of soil moisture layers */
 #define MAX_NODES       50     /**< maximum number of soil thermal nodes */
 #define MAX_BANDS       10     /**< maximum number of snow bands */
